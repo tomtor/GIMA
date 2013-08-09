544e782bf917b90d2fc58eda0a4655c8a75fe36e
Tom Vijlbrief <tvijlbrief@gmail.com>
2013-08-09 15:57:25 +0200

    First query implementation

---
+++ query.py
@@ -0,0 +1,32 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+
+import psycopg2
+import sys
+
+
+con = None
+
+try:
+     
+    con = psycopg2.connect(database='gima', user='tom') 
+    cur = con.cursor()
+    cur.execute('SELECT version()')          
+    ver = cur.fetchone()
+    print ver    
+
+    while True:
+        q = raw_input("\nEnter query: ")
+        cur.execute("SELECT * FROM documenten where lexemes @@ to_tsquery('dutch', '" + q + "')")
+        doc = cur.fetchone()
+        print doc
+    
+except psycopg2.DatabaseError, e:
+    print 'Error %s' % e    
+    sys.exit(1)
+    
+    
+finally:
+    
+    if con:
+        con.close()
