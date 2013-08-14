89b5fdafd22d7ba95251fce7fe919fba5067d7db
Tom Vijlbrief <tvijlbrief@gmail.com>
2013-08-14 13:45:51 +0200

    Add python 3 load script which also adds GIN index

---
+++ load_pgindex.py
@@ -0,0 +1,44 @@
+#!/usr/bin/python3
+# -*- coding: utf-8 -*-
+
+import psycopg2
+import sys
+
+
+con = None
+
+try:
+    con = psycopg2.connect(database='gima', user='tom') 
+    cur = con.cursor()
+
+    cur.execute("DROP TABLE documenten")
+    cur.execute("CREATE TABLE documenten(id SERIAL PRIMARY KEY, lexemes TSVECTOR, doc TEXT, bron VARCHAR(100))")
+
+    doc = ''
+    f = open('data/geboorte.txt', 'r')
+    for line in f:
+        if len(line) == 1:
+            doc = doc.translate(str.maketrans('', '', '"\''))
+            print(doc)
+            cur.execute("INSERT INTO documenten(lexemes,doc,bron) VALUES(" \
+                + "to_tsvector('dutch', '" + doc + "'), '" + doc + "', 'demo')")
+            doc = ''
+        else:
+            doc = doc + line
+    f.close()
+    
+    con.commit()
+
+    cur.execute("CREATE INDEX documenten_idx ON documenten USING gin(to_tsvector('dutch', 'lexemes'))")
+    con.commit()
+
+    con.set_isolation_level(0)
+    cur.execute("VACUUM")
+    
+except psycopg2.DatabaseError as e:
+    print('Error %s' % e)
+    sys.exit(1)
+    
+finally:
+    if con:
+        con.close()
